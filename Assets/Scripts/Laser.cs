5d147cd7edc5df54e3eb99561720fa7344825456
Kevin Erdiza Yogatama <kyogatama@gmail.com>
2018-01-27 01:12:23 +0700

    laser experiment

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -1,37 +1,143 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
-public class Laser : MonoBehaviour {
-	LineRenderer line;
+public class Laser : MonoBehaviour
+{
+    LineRenderer line;
+    public enum Direction
+    {
+        Up,
+        Right,
+        Down,
+        Left
+    }
 
-	void Start() {
-		line = GetComponent<LineRenderer>();
-		line.positionCount = 2;
-		line.SetPosition(0,transform.position);
-		line.SetPosition(1,transform.position);
-	}
+    public Direction initialDirection;
+    Vector2 laserDirection;
+    public float laserLength = 10f;
 
-	void Update() {
-		RaycastHit2D hit = Physics2D.Raycast(transform.position,transform.position - Input.mousePosition,50);
-		line.SetPosition(1,hit.point);
-		if(hit && hit.collider.GetComponent<Mirror>()) {
-			Vector3 newDirection = Vector3.Reflect(Vector3.left, hit.normal);
-			RaycastHit2D nextHit = Physics2D.Raycast(hit.point + 5* Vector2.right,newDirection,50);
-			line.positionCount = 3;
-			line.SetPosition(2,new Vector2(2,-10));
-		} else {
-			line.positionCount = 2;
-		}
-	}
+    void OnValidate()
+    {
+        SyncDirection();
+    }
 
-	void TowardMouse() {
-		Vector3 mouse_pos = Input.mousePosition;
-		mouse_pos.z = 5.23f; //The distance between the camera and object
-		Vector3 object_pos = Camera.main.WorldToScreenPoint(transform.position);
-		mouse_pos.x = mouse_pos.x - object_pos.x;
-		mouse_pos.y = mouse_pos.y - object_pos.y;
-		float angle = Mathf.Atan2(mouse_pos.y, mouse_pos.x) * Mathf.Rad2Deg;
-		transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
-	}
+    private void SyncDirection()
+    {
+        switch (initialDirection)
+        {
+            case Direction.Up:
+                laserDirection = Vector2.up;
+                break;
+            case Direction.Right:
+                laserDirection = Vector2.right;
+                break;
+            case Direction.Down:
+                laserDirection = Vector2.down;
+                break;
+            case Direction.Left:
+                laserDirection = Vector2.left;
+                break;
+        }
+    }
+
+    void Start()
+    {
+        line = GetComponent<LineRenderer>();
+        line.positionCount = 2;
+    }
+
+    void Update()
+    {
+        SyncDirection();
+        line.SetPosition(0, transform.position);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, laserDirection, laserLength);
+        if (hit)
+        {
+            bool keepReflecting = true;
+            RaycastHit2D currentHit = hit;
+            while (keepReflecting)
+            {
+                line.SetPosition(line.positionCount - 1, currentHit.point);
+                Mirror mirror = currentHit.collider.GetComponent<Mirror>();
+                if (mirror)
+                {
+                    Vector2 nextDirection = GetNextDirection(mirror.direction);
+                    laserDirection = nextDirection;
+                    RaycastHit2D nextHit = Physics2D.Raycast((Vector2)currentHit.point + nextDirection * 1, nextDirection, laserLength);
+                    if (nextHit)
+                    {
+						if (Vector2.Distance(nextHit.point,currentHit.point) > 0.01f) {
+							currentHit = nextHit;
+							line.positionCount += 1;
+						} else {
+							keepReflecting = false;
+						}
+                    }
+                    else
+                    {
+                        keepReflecting = false;
+                    }
+                }
+                else
+                {
+                    keepReflecting = false;
+                }
+            }
+        }
+        else
+        {
+            line.SetPosition(1, (Vector2)transform.position + laserDirection * laserLength);
+            line.positionCount = 2;
+        }
+    }
+
+    Vector2 GetNextDirection(Mirror.Direction direction)
+    {
+        switch (direction)
+        {
+            case Mirror.Direction.first:
+                if (laserDirection == Vector2.left)
+                {
+                    return Vector2.up;
+                }
+                else if (laserDirection == Vector2.down)
+                {
+                    return Vector2.right;
+                }
+                break;
+            case Mirror.Direction.second:
+                if (laserDirection == Vector2.right)
+                {
+                    return Vector2.up;
+                }
+                else if (laserDirection == Vector2.down)
+                {
+                    return Vector2.left;
+                }
+                break;
+            case Mirror.Direction.third:
+                if (laserDirection == Vector2.right)
+                {
+                    return Vector2.down;
+                }
+                else if (laserDirection == Vector2.up)
+                {
+                    return Vector2.left;
+                }
+                break;
+            case Mirror.Direction.fourth:
+                if (laserDirection == Vector2.left)
+                {
+                    return Vector2.down;
+                }
+                else if (laserDirection == Vector2.up)
+                {
+                    return Vector2.right;
+                }
+                break;
+        }
+        return Vector2.zero;
+    }
 }
--- Assets/Scripts/Mirror.cs
+++ Assets/Scripts/Mirror.cs
@@ -3,4 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Mirror : MonoBehaviour {
+    public enum Direction {
+        first,
+        second,
+        third,
+        fourth
+    }
+
+    public Direction direction;
+
+    void OnValidate() {
+        switch(direction) {
+            case Direction.first:
+                transform.rotation = Quaternion.Euler(0,0,45f);
+                break;
+            case Direction.second:
+                transform.rotation = Quaternion.Euler(0,0,135f);
+                break;
+            case Direction.third:
+                transform.rotation = Quaternion.Euler(0,0,-135f);
+                break;
+            case Direction.fourth:
+                transform.rotation = Quaternion.Euler(0,0,-45f);
+                break;
+        }
+    }
 }
